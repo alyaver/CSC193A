6c951208c26a39b2ce7a47dda26f3cae43556398
alyaver <64661819+alyaver@users.noreply.github.com>
2026-01-31 21:44:33 -0800

    JS now adds -MOO to sentence ends

--- Assignment-5/fancifymytext.js
+++ Assignment-5/fancifymytext.js
@@ -4,9 +4,23 @@ function biggerClicked() {
 
 function setFancy(){
     document.getElementById("text").style.fontWeight = "bold";
+    document.getElementById("text").style.color = "blue";
+    document.getElementById("text").style.textDecoration = "underline";    
 }
 
 function setBoring(){
     document.getElementById("text").style.fontWeight = "normal";
+    document.getElementById("text").style.color = "black";
+    document.getElementById("text").style.textDecoration = "none";    
+}
 
+function mooClicked(){
+    var textArea = document.getElementById("text");
+    textArea.value = textArea.value.toUpperCase(); //uppercase
+
+    var upperCaseTV = textArea.value;
+    var parts = upperCaseTV.split(".");//split where sentence ends
+    upperCaseTV = parts.join("-MOO.");//replace . with -MOO because instructions assume that a sentence ends only with .
+
+    textArea.value = upperCaseTV;//update the page
 }
