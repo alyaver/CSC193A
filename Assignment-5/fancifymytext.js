55461648c3faa3fd3d9b7f2deb9881810ee62d9d
alyaver <64661819+alyaver@users.noreply.github.com>
2026-01-31 21:00:12 -0800

    added ability to change style using the radio buttons

--- Assignment-5/fancifymytext.js
+++ Assignment-5/fancifymytext.js
@@ -1,3 +1,12 @@
 function biggerClicked() {
     document.getElementById("text").style.fontSize = "24pt";
 }
+
+function setFancy(){
+    document.getElementById("text").style.fontWeight = "bold";
+}
+
+function setBoring(){
+    document.getElementById("text").style.fontWeight = "normal";
+
+}
