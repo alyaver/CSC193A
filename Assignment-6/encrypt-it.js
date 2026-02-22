891fb54d48a4f66d7ac35cc8819e050e306179cd
alyaver <64661819+alyaver@users.noreply.github.com>
2026-02-22 14:12:07 -0800

    New Feature, Complete, Final Z wraps to A, Shift Cipher works

--- Assignment-6/encrypt-it.js
+++ Assignment-6/encrypt-it.js
@@ -6,27 +6,43 @@
 
   window.addEventListener("load", init);
 
-  /**
-   * Sets up event listeners once the page loads.
-   */
   function init() {
     let encryptButton = document.getElementById("encrypt-it");
-    encryptButton.addEventListener("click", handleClick);
+    encryptButton.addEventListener("click", handleEncrypt);
 
     let resetButton = document.getElementById("reset");
     resetButton.addEventListener("click", handleReset);
   }
 
-  /**
-   * Handles clicking the Encrypt-It button.
-   */
-  function handleClick() {
-    console.log("Button clicked!");
+  function handleEncrypt() {
+    let inputText = document.getElementById("input-text").value;
+    let encryptedText = shiftCipher(inputText);
+
+    document.getElementById("result").textContent = encryptedText;
   }
 
   function handleReset() {
-    let textArea = document.getElementById("input-text");
-    textArea.value = "";
+    document.getElementById("input-text").value = "";
+    document.getElementById("result").textContent = "";
+  }
+
+  function shiftCipher(text) {
+    text = text.toLowerCase();
+    let result = "";
+
+    for (let i = 0; i < text.length; i++) {
+      if (text[i] < "a" || text[i] > "z") {
+        result += text[i];
+      } else if (text[i] === "z") {
+        result += "a";
+      } else {
+        let letter = text.charCodeAt(i);
+        let resultLetter = String.fromCharCode(letter + 1);
+        result += resultLetter;
+      }
+    }
+
+    return result;
   }
 
 })();
